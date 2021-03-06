9b7fada0c3ea0b21797e0c881e630e67fec4625b
Vinayak agarwal <vinayakaggarwal05@gmail.com>
2021-03-06 14:10:55 +0530

    code done

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,13 +16,21 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,8 +47,34 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val nums = remember { mutableStateOf(10L) }
+    val text = remember { mutableStateOf("count down") }
+    val countNum = object : CountDownTimer(
+        10000,
+        1000
+    ) {
+        override fun onTick(millisUntilFinished: Long) {
+            nums.value = millisUntilFinished / 1000
+            text.value = "time remaining ${nums.value}"
+        }
+
+        override fun onFinish() {
+            text.value = "time up"
+        }
+    }
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "${text.value}",
+                modifier = Modifier.padding(16.dp)
+            )
+            Button(onClick = { countNum.start() }) {
+                Text(text = "Start countdown numbers")
+            }
+        }
     }
 }
 
